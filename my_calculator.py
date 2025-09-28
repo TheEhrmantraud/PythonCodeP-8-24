50ecebcf9ee52e3ead7278b54c7a02b4faa8a4ee
TheEhrmantraud <viketkykin@gmail.com>
2025-09-28 10:28:51 +0300

    Добавил блок indentity, проверочка значений на is & is not. Коммент для себя: С большим трудом и при помощи инета сделал, тк для меня ast & literal_eval это что-то новое. Почитаю мб об этом еще, когда время будет

--- my_calculator.py
+++ my_calculator.py
@@ -1,3 +1,5 @@
+import ast
+
 def arifmet():
     a=int(input('a: '))
     b=int(input('b: '))
@@ -18,7 +20,7 @@ def arifmet():
 
 # Важные штучки
 def for_bool(x):    # -> тута проверяемс циферки на True/False
-    x=input('что-то')
+    x=input('что-то').strip().lower()
     x=x.strip
     if x in ('true', 't', '1', 'yes', 'y'): return True
     if x in ('false', 'f', '0', 'no', 'n'): return False
@@ -61,12 +63,22 @@ def sravneniya():
     except Exception:
         print('Нельзя сранвить')
 
-
+def identity():
+    try:    
+        a=ast.literal_eval(input('a (литералка, напр. 10, "hi", [1,2]): '))
+    except:
+        a=input('Строчка a: ')
+    try:    
+        b=ast.literal_eval(input('b (литералка, напр. 10, "hi", [1,2]): '))
+    except:
+        b=input('Строчка b: ')
+    pop=input('is или is not?').strip().lower()
+    if pop=='is': print(a is b)
+    elif pop=='is not': print(a is not b)
+    else: print('Неверная операция')
 
 
     #Здесь самый сок будет, но потом
-#def logic():
-#def logic():
 #def logic():
     #Здесь самый сок будет, но потом
 
@@ -74,13 +86,12 @@ def sravneniya():
 def main():         # -> Корень кода (кальки), сюда все def'ки подтягиваются, которые написаны (должны быть) выше
     while True:
         print('Привет, введи исполнимую операцию: ')
-        num = input()
+        num = input().strip()
         if num == '1': arifmet()
         elif num == '2': logic()
-        elif num == '3': sravneniya ()
-        #elif num == '4': 
-        #elif num == '5':
-        #elif num == '6':
+        elif num == '3': sravneniya()
+        elif num == '4': identity()
+        #elif num == '5': 
         else: print('Говно число')
 
 if __name__=="__main__":    # -> Запук из под корня, трогает сразу main
